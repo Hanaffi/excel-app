a7f7163f497ca58b177ff2ecbf6ede07f2bdc6fd
Ahmed <ahmedellord56@gmail.com>
2020-09-11 18:34:32 +0200

    Updates

---
+++ precache-manifest.c24b5fc404e4bb23219b98ed9ea5f2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec9bb66f21de05368c8dc0bd68d4117",
+    "url": "/excel-app/index.html"
+  },
+  {
+    "revision": "c4b7acb7acfcea05a19f",
+    "url": "/excel-app/static/css/main.197b6be4.chunk.css"
+  },
+  {
+    "revision": "a903e1b8f032859213f8",
+    "url": "/excel-app/static/js/2.ffd1124d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/excel-app/static/js/2.ffd1124d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b7acb7acfcea05a19f",
+    "url": "/excel-app/static/js/main.764f5afc.chunk.js"
+  },
+  {
+    "revision": "9e4a1e0cab80d32c6a20",
+    "url": "/excel-app/static/js/runtime-main.220a8742.js"
+  }
+]);
